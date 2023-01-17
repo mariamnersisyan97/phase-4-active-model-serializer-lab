a408dffc0d9d2f70fe29819e06c3632c8578ef68
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2023-01-17 17:49:57 -0500

    commit tree f6c4aeb557119d04b5cd7120335a2fec8075c5f4

---
+++ app/controllers/concerns/profiles_controller.rb
@@ -0,0 +1,21 @@
+class ProfilesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
+    def index
+      profile = Profile.all 
+      render json: profile
+    end
+  
+    def show
+      profile = Profile.find(params[:id])
+      render json: profile
+    end
+  
+    private
+  
+    def render_not_found_response
+      render json: { error: "Author not found" }, status: :not_found
+    end
+  
+  end
+  
